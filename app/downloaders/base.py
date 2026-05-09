43240412b05b844bbe5c1eaa904656ada507f260
evgcursed <evgcursed@gmail.com>
2026-05-10 00:32:36 +0500

    refactor

--- app/downloaders/base.py
+++
@@ -1,6 +0,0 @@
-import httpx
-
-
-class BaseDownloader:
-    def __init__(self, session: httpx.AsyncClient) -> None:
-        self.session: httpx.AsyncClient = session
--- app/httpx_client.py
+++
@@ -1,7 +0,0 @@
-import httpx
-
-
-class HttpxClient:
-    def __init__(self, headers: dict) -> None:
-        self.headers = headers
-        self.session = httpx.AsyncClient(headers=headers)
